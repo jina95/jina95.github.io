652c8fedf0881ca185c6171f680087f019f46c61
jinalee <spzn02@gmail.com>
2020-05-24 23:30:15 +0900

    casetify & update 🙌

---
+++ casetify/js/common.js
@@ -0,0 +1,282 @@
+// 재료 준비
+const descMenu = document.querySelector('.desc_menu');
+const descMenuEtc = document.querySelector('.header_etc')
+const headerSearch = descMenuEtc.querySelector('.header_search')
+const headerCart = descMenuEtc.querySelector('.header_cart')
+const mobileMenu = document.querySelector('.header_mobile')
+
+
+tabWork()
+function tabWork(){
+    // console.log(window.event.keyCode);
+    
+    // if(window.e)
+    // window.onkeydown = function(e){
+    //     console.log(e.key);
+        
+        // if(!KeyboardEvent.key === 'Tab') return;
+        // console.log(e);
+        
+        // if(!.keyCode === 9)return
+        // console.log(window.event.keyCode);
+        
+    // }
+    // let aEls = descMenu.querySelector('li > a')
+    // let ulEls = descMenu.querySelector('ul')
+    // for (let i = 0; i < aEls.length; i++) {
+    //     aEls[i].onkeydown = function(e){
+    //         console.log(this);
+            
+    //     }
+    //     // aEls[i].focus(ulEls[i].style.display = 'block')
+    // };
+}
+
+
+descMenuFun() // pc버전 menu header 오픈 함수
+function descMenuFun(){
+    let openedMenu;
+    descMenu.addEventListener('mouseover', overWork);
+    descMenu.addEventListener('mouseout', outWork);
+
+    function overWork(e){
+        if(headerSearch.querySelector('.on')) return
+        
+        let SelectDivMenu = e.target.parentNode.querySelector('div');
+        if (SelectDivMenu) {
+            SelectDivMenu.classList.add('on');
+            openedMenu = SelectDivMenu;
+        }
+    };
+
+    function outWork(e) {
+        if (openedMenu === undefined || e.relatedTarget === null ) return;
+        if (e.relatedTarget.className === 'header_menu_title') {
+            openedMenu.classList.remove('on');
+        }  
+        if (!isDiv (e.relatedTarget)) { // 도착한 곳이 div 인지 확인해라.
+            openedMenu.classList.remove('on');
+        }
+
+        function isDiv(arriveMenu) {
+            if (arriveMenu === null) return;
+            while (arriveMenu.tagName !== 'HTML' ) {
+                if (arriveMenu === descMenu ) { //descmenu 의 자식이라면, 닫지 말아라.
+                    return true;
+                }
+               arriveMenu = arriveMenu.parentNode; 
+            }
+            return false;
+        }
+    }
+    // 컬렉션 옆으로 포물선을 그리며 옆으로 가면 사라지는데 직선으로 가면 헤드가 안 닫힘.
+}
+
+headerEtcOverFun(); // header flag & cart work
+function headerEtcOverFun(){
+    const headerFlag = descMenuEtc.querySelector('.header_lang')
+    const headerCart = descMenuEtc.querySelector('.header_cart')
+    let openedMenu;
+
+
+    headerFlag.addEventListener('mouseover', overWork);
+    headerFlag.addEventListener('mouseout', outWork);
+    headerCart.addEventListener('mouseover', overWork);
+    headerCart.addEventListener('mouseout', outWork);
+
+        
+    // flag & cart 둘다 mouseover event 를 사용하기 때문에 한 함수로 묶으려고 노력하였다.
+    function overWork(e){
+        if(descMenu.querySelector('.on') || headerSearch.querySelector('.on')) return; // 헤더의 자식들 중 on 이 들어가있는애가 있는 상태로 여기에 온다면 (?) return 해라.
+        let target = e.target
+        if(target === headerFlag || target.title === 'Language'){ // headerFlag에 mouseover 한다면,
+            let SelectUl = target.parentNode.querySelector('ul'); // 그밑에 ul 한테 on 을 줘라.
+            SelectUl.classList.add('on');
+            openedMenu = SelectUl;
+            
+        } else if(target === headerCart || target.alt === 'cart'){ // 만약 cart 아이콘에 over 가 닿는다면
+            headerCart.classList.add('on');// 헤더카트에 on 을 줘라.
+        } 
+    }
+
+    function outWork(e) {
+        if (openedMenu === undefined || e.relatedTarget === null) return;
+        if (!isRight (e.relatedTarget)) { // 자식 확인함수 = isRight 
+            if(openedMenu.classList.contains('on'))openedMenu.classList.remove('on') // cart on 제거
+            else headerCart.classList.remove('on'); // flag on 제거 
+            
+            
+        }
+        
+        function isRight(arrived) {
+            if (arrived === null) return;
+            while (arrived.tagName !== 'HTML' ) { // html 까지 올라가서 확인해라.
+                if (arrived === headerFlag || arrived === headerCart ) {
+                    return true;
+                }
+                arrived = arrived.parentNode; 
+            }
+            return false;
+        }
+    }
+    // headercart 에서 나온애가 안꺼진 상태에서 headerflag 로가면 cart 창이 꺼지고 flag 가 켜져야하는데,
+    // 지금은 겹쳐지는 상황이다.
+}
+
+// 이 밑은 flag 와 cart 를 따로 준 함수인데, 겹치는 부분이 있지만 다른부분이 있어서 인지 밑에처럼 해야 오류는 덜하나
+// 코드적으로 깔끔하게 중복되지 않게 하려면 headerEtcOverFun() 로 묶는게 좋은데, 고민해 봐야 하는 문제일 듯 싶다.
+
+
+// flagFun();
+// function flagFun(){
+//     const headerFlag = descMenuEtc.querySelector('.header_lang')
+
+//     headerFlag.addEventListener('mouseover', overWork)
+//     headerFlag.addEventListener('mouseout', outWork)
+//     let openedMenu;
+//     function overWork(e){
+//         if(descMenu.querySelector('.on') || headerSearch.querySelector('.on')) return;
+//         let SelectUl = e.target.parentNode.querySelector('ul');
+//         if (SelectUl) {
+//             SelectUl.classList.add('on');
+//             openedMenu = SelectUl;
+//         } 
+//     }
+
+//     function outWork(e) {
+//         if (openedMenu === undefined || e.relatedTarget === null) return;
+
+//         if (!isDiv (e.relatedTarget)) {
+//             openedMenu.classList.remove('on');
+//         }
+        
+//         function isDiv(arrived) {
+//             if (arrived === null) return;
+//             while (arrived.tagName !== 'HTML' ) {
+//                 if (arrived === headerFlag ) {
+//                     return true;
+//                 }
+//                 arrived = arrived.parentNode; 
+//             }
+//             return false;
+//         }
+//     }
+// }
+
+// cartFun();
+// function cartFun(){
+//     headerCart.addEventListener('mouseover', cartOver);
+//     headerCart.addEventListener('mouseout', cartEnter);
+//     function cartOver(){
+//         if(descMenu.querySelector('.on') || headerSearch.querySelector('.on')) return;
+//         headerCart.classList.add('on');
+//     }
+//     function cartEnter(e){
+//         if (!isDiv (e.relatedTarget)) {
+//             headerCart.classList.remove('on');
+//         }
+//         function isDiv(arrived) {
+//             if (arrived === null) return;
+//             while (arrived.tagName !== 'HTML' ) {
+//                 if (arrived === headerCart ) {
+//                     return true;
+//                 }
+//                 arrived = arrived.parentNode; 
+//             }
+//             return false;
+//         }
+//     }
+// }
+
+
+
+headerSearchWork() // 헤더 서치아이콘 work
+function headerSearchWork(){
+    const headerSearchForm = headerSearch.querySelector('form');
+    const headerSearchInput = headerSearch.querySelector('form > input');
+
+    headerSearch.addEventListener('click', clickEventFun); // 인풋창 클릭 함수
+    headerSearchInput.addEventListener('keyup', searchKeyfun); // 인풋창에 글자 입력했을때 함수
+
+    function clickEventFun(e) {
+        e.preventDefault();
+
+        if(descMenu.querySelector('.on')) return; // 헤더의 자식들 중 on 이 들어가있는애가 있는 상태로 클릭을 한다면 (?) return 해라.
+        const serachImg = headerSearch.querySelector('img')
+        const searchBox = headerSearch.querySelector('.search_box')
+        
+        if(!searchBox.classList.contains('on')){ // searchBox를 눌렀을 때 search div 가 안켜져있는 상태라면 
+            searchBox.classList.add('on') // 키워라
+        } else if(e.target === headerSearch || e.target === serachImg ) { // 하지만 누른애가 돋보기모양의 아이콘이라면
+            searchBox.classList.remove('on') // 지워라
+        } else if(!isSearchBox(e.target)){ // 또한 도착한애가 headerSearch의 자식이라면
+            searchBox.classList.remove('on') // 지워라 (div 안에서 input 같은 요소들을 눌렀을때 지워지면 안되기 때문.)
+        }
+
+        function isSearchBox(arrived){ // 도착한애의 부모가 누군지 확인하는 함수.
+            if (arrived === null) return; // 도착한곳이 null 이라면 돌아가라.
+            while (arrived.tagName !== 'HEADER' ) { // parentNode가 HEADER일때까지 확인해라.
+                if (arrived === headerSearch ) {
+                    return true; 
+                }
+                arrived = arrived.parentNode; 
+            }
+            return false;   
+        }
+    }
+
+    function searchKeyfun(e){
+        const headerSearchXBox = headerSearchForm.querySelector('.xbox_icon')
+        headerSearchForm.classList.add('x_box'); // keyup 이 일어났을때 x아이콘을 on 해라.
+        headerSearchXBox.addEventListener('click', function(){ // x아이콘을 눌렀을때는,
+            headerSearchInput.value = ''; //값을 지우고
+            headerSearchForm.classList.remove('x_box'); //x 아이콘을 없애라.
+        })
+        
+    }
+}
+
+
+mobileMenuFun(); // 모바일메뉴 work
+function mobileMenuFun(){
+    const bodyEl = document.querySelector('body')
+    const hamburgerIcon = mobileMenu.querySelector('.hamburger_menu');
+    const menuLis = mobileMenu.querySelectorAll('ul > li');
+    const mobileFlag = mobileMenu.querySelector('.flag_lang')
+
+    hamburgerIcon.addEventListener('click', mobileMenuWork) // 햄버거메뉴 오픈 work
+
+    for (let i = 0; i < menuLis.length; i++) {
+        menuLis[i].addEventListener('click',mobileListWork) // 메뉴 안의 li open work
+    }
+    mobileFlag.addEventListener('click', mobileFlagClickWork)
+
+    function mobileMenuWork(e){ // 햄버거메뉴 클릭 
+        e.preventDefault();
+        if(mobileMenu.classList.contains('on')){
+            mobileMenu.classList.remove('on');
+            bodyEl.classList.remove('not_scroll') // 모바일메뉴가 닫히면 스크롤 원상복구!
+        } else {
+            mobileMenu.classList.add('on');
+            bodyEl.classList.add('not_scroll') // 모바일 메뉴가 오픈 되었을때 스크롤을 막아라.
+        }
+        
+    }
+    function mobileListWork(e){ // 모바일메뉴 li 클릭 
+        e.preventDefault();
+        if (this.className === 'on') {
+            this.className = ''
+        } else {
+            this.className = 'on'
+        }  
+    }
+    function mobileFlagClickWork(e){ // 모바일 언어 클릭
+        if(e.target.title !== 'Language') return 
+        let flagList = e.target.parentNode.querySelector('ul')
+        if (flagList.classList.contains('on')){
+            flagList.classList.remove('on')
+        } else {
+            flagList.classList.add('on')
+        }
+    }
+}
---
+++ casetify/js/slide.js
@@ -0,0 +1,175 @@
+
+
+let sizeCheck;
+const slideList = document.querySelector('.main_slide'); 
+let firstChild = slideList.firstElementChild;
+let slideWidth = firstChild.clientWidth;
+const slideContents = document.querySelectorAll('.slider_item');
+const slideLen = slideContents.length;
+// const slideSpeed = 1000; > 이게맞는속도
+const slideSpeed = 1000;
+const startNum = 0;
+const slidePage = document.querySelector('.slide_btn');
+// const pageDots = slidePage.querySelectorAll('li');
+
+
+
+
+
+slideList.style.width = slideWidth * (slideLen + 2) + 'px'; //ul 넓이 지정 (복사본까지 합친 넓이.)
+
+//자연스러운 슬라이드를 위해 양 끝에 슬라이드 복제하기 - <4번> 1번 2번 3번 4번 <1번>
+let lastChild = slideList.lastElementChild;
+let cloneFirst = firstChild.cloneNode(true);
+let cloneLast = lastChild.cloneNode(true);
+
+slideList.appendChild(cloneFirst);
+slideList.insertBefore(cloneLast,slideList.firstElementChild);
+
+slideList.style.transform = `translateX(-${slideWidth * ( startNum + 1 )}px)` // 복사본부터 시작해야하기때문에 원래 슬라이드 아이템보다 -slidewidth된 상태에서 시작해야한다.
+
+// page navi
+let pageChild = '';
+for (var i = 0; i < slideLen; i++) {
+    pageChild += '<li class="dot';
+    pageChild += (i === startNum) ? ' dot_active' : '';
+    pageChild += '" data-index="' + i + '"><a href="#"></a></li>';
+}
+slidePage.innerHTML = pageChild;
+const pageDots = document.querySelectorAll('.dot');
+
+let curDot;
+Array.prototype.forEach.call(pageDots, function (dot, i) {
+    dot.addEventListener('click', function (e) {
+    e.preventDefault();
+    curDot = document.querySelector('.dot_active');
+    curDot.classList.remove('dot_active');
+    curDot = this;
+    this.classList.add('dot_active');
+    curSlide.classList.remove('slide_active');
+    curIndex = Number(this.getAttribute('data-index'));
+    curSlide = slideContents[curIndex];
+    curSlide.classList.add('slide_active');
+    slideList.style.transition = slideSpeed + "ms";
+    slideList.style.transform = "translate3d(-" + (slideWidth * (curIndex + 1)) + "px, 0px, 0px)";
+    });
+});
+
+
+let curIndex = startNum;
+let curSlide = slideContents[curIndex];
+curSlide.classList.add('slide_active');
+pageDots[curIndex].classList.add('dot_active');
+
+// setInterval(slidePlay,5000); > 이게 맞는 속도
+// setInterval(slidePlay,4000);
+setInterval(slidePlay,4000);
+
+function slidePlay() {
+    if (curIndex <= slideLen - 1) {
+      slideList.style.transition = slideSpeed + "ms";
+      slideList.style.transform = "translate3d(-" + (slideWidth * (curIndex + 2)) + "px, 0px, 0px)";
+    }
+    if (curIndex === slideLen - 1) {
+      setTimeout(function() {
+        slideList.style.transition = "0ms";
+        slideList.style.transform = "translate3d(-" + slideWidth + "px, 0px, 0px)";
+      }, slideSpeed);
+      curIndex = -1;
+    }
+    curSlide.classList.remove('slide_active');
+    pageDots[(curIndex === -1) ? slideLen - 1 : curIndex].classList.remove('dot_active');
+    curSlide = slideContents[++curIndex];
+    curSlide.classList.add('slide_active');
+    pageDots[curIndex].classList.add('dot_active');
+  };
+
+// function slidePlay(){
+//     if( curIndex <= slideLen - 1){
+//         slideList.style.transition = `all ${slideSpeed}ms ease`;
+//         slideList.style.transform = `translateX(-${slideWidth * (curIndex + 2)}px)`;
+//     }
+
+//     if( curIndex === slideLen - 1){ // 마지막 아이템일때는 트렌지션을 0 하고 첫번째복제본으로 돌려줘야한다.
+//         setTimeout(function(){
+//             slideList.style.transition ='0ms';
+//             slideList.style.transform = `translateX(-${slideWidth}px)`;
+                
+//         },slideSpeed)
+        
+//         curIndex = -1;
+//     }
+//     curSlide.classList.remove('slide_active');
+//     // pageDots[curIndex].classList.remove('dot_active');
+//     curIndex++;
+//     curSlide = slideContents[curIndex];
+//     curSlide.classList.add('slide_active');
+//     pageDots[curIndex].classList.add('dot_active');
+
+//     console.log(curIndex);
+// }
+
+window.addEventListener('resize', function(){
+    clearTimeout(sizeCheck);
+    sizeCheck = setTimeout(function(){
+        slideWidth = firstChild.clientWidth;
+        slideList.style.width = slideWidth * (slideLen + 2) + 'px';
+        slideList.style.transform = `translateX(-${slideWidth * ( startNum + 1 )}px)`;
+        // console.log(startNum);
+        
+        //크기가 커진만큼 translateX가 되야한다. ( 그 다음슬라이드는 해당이 되는데 당장 화면을 늘린 당시 슬라이드가 적용이 안된다.)
+        // 크기가 처음 이외에 두번째에는 뒤로가기도하고 두번 넘겨지기도 함. 
+        // curIndex 가 현재 슬라이드 부터 ... 기존의 것을 따라가기 위해 위에처럼 두번넘겨지고 뒤로가고 하는듯 하다.
+    }, 100);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let curDot;
+
+// for (let i = 0; i < pageDots.length; i++) {
+//     pageDots[i].data_index = i
+//     pageDots[i].addEventListener('click', pageDotsWork) 
+//     // console.log(pageDots[i].data_index);
+
+// }
+
+
+
+function pageDotsWork(e){
+    e.preventDefault();
+    curDot = document.querySelector('.dot_active');
+    curDot.classList.remove('dot_active');
+    curDot = this;
+    this.classList.add('dot_active');
+    curSlide.classList.remove('slide_active');
+    curIndex = this.data_index - 1 
+    // curIndex = Number(this.getAttribute('data-index'));
+    console.log(curIndex);
+    curSlide = slideContents[curIndex];
+    // curSlide.classList.add('slide_active');
+    // slideList.style.transition = slideSpeed + "ms";
+    // slideList.style.transform = `translateX(-${slideWidth * ( curIndex + 1 )}px)`;
+    slidePlay();
+}
+
+
+
+
+
+
+
